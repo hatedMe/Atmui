315d645e73dfed25517261b203f0bcbfae8d384c
Atom <7548764@qq.com>
2017-12-29 14:49:19 +0800

    add util

--- src/components/Calendar/Calendar.vue
+++ src/components/Calendar/Calendar.vue
@@ -4,13 +4,37 @@
 
 
 <template>
-    <Calendar></Calendar>
+    <div>
+        <Button type="success" licon="icon-like" @click="clickHandle">点击</Button>
+        <form ref="mrform">
+            <input type="text" name="age" />
+            <input type="text" name="num" />
+            <button type="submit">tijiao</button>
+        </form>
+    </div>
 </template>
 
 
 
 <script>
+    //import params from '../../../lib/util/params.js'
     export default {
-        name: 'calendar'
+        name: 'calendar',
+        methods: {
+            clickHandle() {
+                let obj = {
+                    a: '1',
+                    b: {
+                        name: '456',
+                        age: '24'
+                    },
+                    c: 'xiaomin',
+                    d: 24,
+                    e: [1, 2, '3']
+                }
+
+                console.log(this.$Atmui.params(this.$refs.mrform));
+            }
+        }
     }
 </script>
