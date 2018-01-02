55fd2bf644f2620e75f59f6696d3491755255416
Admin <7548764@qq.com>
2018-01-02 22:42:30 +0800

    add TouchEvent

--- lib/components/Calendar/Calendar.vue
+++ lib/components/Calendar/Calendar.vue
@@ -25,7 +25,7 @@
                 <span>五</span>
                 <span>六</span>
             </div>
-            <div class="calendar-day flex" @click="changeDay" v-focusss="touchHandle">
+            <div class="calendar-day flex" @click="changeDay" v-tap:swiperight="{methods : touchHandle}">
                 <span class="no-date" v-for="item in datt" v-if="item.wk == undefined && item.today == undefined">{{ item.day }}</span>
                 <span class="active_wk" v-else-if="(item.wk != undefined) && (item.wk == 0 || item.wk == 6)">{{ item.day }}</span>
                 <span class="today active" v-else-if="item.today != undefined">{{ item.day }}</span>
@@ -39,7 +39,7 @@
 
 <script>
     import datt from './Calendar.js';
-    import TouchEvent from '../../util/TouchEvent.js';
+
     export default {
         name: 'Calendar',
         data () {
@@ -48,13 +48,9 @@
             }
         },
         mounted () {
-            //this.datt = datt();
         },
         methods: {
             changeDay(event){
-                // TouchEvent(document , 'tap' , function(){
-                //     console.log( 789  );
-                // });
                 let els = event.target.parentNode.children;
 
                 for( let i =0; i<els.length; i++ ){
