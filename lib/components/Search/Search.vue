00c483e2407f09c015004fb77fd0cea3a87bc7c1
Admin <7548764@qq.com>
2018-01-11 22:40:56 +0800

    search less && search vue :beers:

---
+++ lib/components/Search/Search.vue
@@ -0,0 +1,20 @@
+
+
+<template>
+    <div class="input-search">
+        <div>
+            <input type="text" />
+            <Icon />
+        </div>
+        <p class="search-cancel"></p>
+    </div>
+</template>
+
+
+
+
+<script>
+    export default {
+        name: 'Search'
+    }
+</script>
