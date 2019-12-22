d8c1bf1595922af6d928432da6cb685dab794856
Atom <7548764@qq.com>
2019-12-23 01:35:58 +0800

    Improved code structure and format :art:

---
+++ package/main.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        holle
+    </div>
+</template>
+
+<script>
+export default {
+    name : 'main'
+}
+</script>
+
+<style lang="less">
+body {
+    font-size: 14px;
+}
+</style>
---
+++ vue.config.js
@@ -0,0 +1,17 @@
+const path = require('path');
+function resolve(dir) {
+    return path.resolve(__dirname, dir)
+}
+module.exports = {
+	css: {
+		extract: true,
+    },
+    productionSourceMap: false,
+	configureWebpack : config => {
+        Object.assign( config , {
+            output: {
+                libraryExport: 'default'
+            }
+        });
+    },
+}
